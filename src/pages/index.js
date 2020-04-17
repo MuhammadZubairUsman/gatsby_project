68a3841b3d1e7545e0b3e2ee740bf833b016d293
MuhammadZubairUsman <muhammadzubair900@outlook.com>
2020-04-17 21:56:15 +0500

    Errors removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi Rana Usman Amjad</h1>
     <p>Welcome to My Website.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
     <Link to="/services/">Go to page 2</Link>
   </Layout>
